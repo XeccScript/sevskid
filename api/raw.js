8bce527367ceac5cf60bc7902ff38a8b41693bff
XeccScript <photocomment0918@gmail.com>
2025-11-27 22:45:19 +0800

    Change error message for unauthorized access

--- api/raw.js
+++ api/raw.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   const id = req.url.split("/").pop().split("?")[0];
 
   if (!ua.includes("roblox")) {
-    res.status(403).send("❌ Bạn không có quyền xem code này");
+    res.status(403).send("sev skidder😂");
     return;
   }
 
